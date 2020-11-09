cee5758e8e55aff996cdacd7fa775ab18c6d1f60
Sumaya Hassan <sumaya_198@hotmail.co.uk>
2020-11-09 22:56:33 +0000

    second commit

---
+++ script/questions.js
@@ -0,0 +1,76 @@
+let questions = [
+    {
+        number: 1,
+        question: "What does HTML stand for?",
+        answer: "Hyper Text Markup Language",
+        options: [
+            "Hyper Text Markup Language",
+            "Hyperlinks and Text Markup Language",
+            "Home Tool Markup Language",
+            "Hyper Tool Markup Language"]
+     },
+     {
+        number: 2,
+        question: "What does XML stand for?",
+        answer: "eXtensible Markup Language",
+        options: [
+          "eXtensible Markup Language",
+          "eXecutable Multiple Language",
+          "eXTra Multi-Program Language",
+          "eXamine Multiple Language"
+        ]
+      },
+        
+    {
+        number: 3,
+        question: "What does SQL stand for?",
+        answer: "Structured Query Language",
+        options: [
+          "Stylish Question Language",
+          "Stylesheet Query Language",
+          "Statement Question Language",
+          "Structured Query Language"]
+        },
+        {
+            number: 4,
+            question: "What does CSS stand for?",
+            answer: "Cascading Style Sheet",
+            options: [
+              "Common Style Sheet",
+              "Colorful Style Sheet",
+              "Computer Style Sheet",
+              "Cascading Style Sheet"
+            ]
+          },
+        {
+        number: 5,
+        question: "Who is making the Web standards?",
+        answer: "The World Wide Web Consortium",
+        options: [
+            "Microsoft",
+            "Google",
+            "Mozilla",
+            "The World Wide Web Consortium"]
+        },
+        {
+        number: 6,
+        question: "Which bootstrap class provides a responsive fixed width container?",
+        answer: ".container-fluid",
+        options: [
+            ".container",
+            ".container-fixed",
+            ".container-fluid",
+            ".row"]
+        },
+        {
+        number: 7,
+        question: "The Bootstrap grid system is based on how many columns?",
+        answer: "12",
+        options: [
+            "3",
+            "6",
+            "12",
+            "9"]
+        
+        },]; 
+    
